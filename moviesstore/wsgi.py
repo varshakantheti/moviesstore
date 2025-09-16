c422e86d8c8af3c673308404df178341e56798c4
Kantheti, Varsha <vkantheti3@gatech.edu>
2025-09-16 08:12:31 -0400

    test

--- moviesstore/wsgi.py
+++ moviesstore/wsgi.py
@@ -5,6 +5,8 @@ It exposes the WSGI callable as a module-level variable named ``application``.
 
 For more information on this file, see
 https://docs.djangoproject.com/en/5.0/howto/deployment/wsgi/
+
+django stuff
 """
 
 import os
